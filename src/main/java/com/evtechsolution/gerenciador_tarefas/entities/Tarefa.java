f1a807d763f10b7504bd8d3af7e13792a9dea79f
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-12 00:36:19 -0300

    colocando annotation em Tarefa e definindo o Strategy

--- src/main/java/com/evtechsolution/gerenciador_tarefas/entities/Tarefa.java
+++ src/main/java/com/evtechsolution/gerenciador_tarefas/entities/Tarefa.java
@@ -5,8 +5,17 @@ import java.util.Objects;
 
 import com.evtechsolution.gerenciador_tarefas.entities.enums.Status;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Tarefa {
 	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	String titulo;
 	String descricao;
