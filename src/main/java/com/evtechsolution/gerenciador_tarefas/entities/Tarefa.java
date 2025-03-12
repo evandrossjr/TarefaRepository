207d1e2072cbec309f31bcf17aee8647967a6588
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-11 22:21:41 -0300

    primeiro commit na main

---
+++ src/main/java/com/evtechsolution/gerenciador_tarefas/entities/Tarefa.java
@@ -0,0 +1,77 @@
+package com.evtechsolution.gerenciador_tarefas.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.evtechsolution.gerenciador_tarefas.entities.enums.Status;
+
+public class Tarefa {
+	
+	Long id;
+	String titulo;
+	Status status;
+	LocalDateTime dataCriacao;
+
+	public Tarefa() {
+		
+	}
+
+	public Tarefa(Long id, String titulo, Status status, LocalDateTime dataCriacao) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.status = status;
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Long getid() {
+		return id;
+	}
+
+	public void setid(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
---
+++ src/main/java/com/evtechsolution/gerenciador_tarefas/entities/enums/Status.java
@@ -0,0 +1,8 @@
+package com.evtechsolution.gerenciador_tarefas.entities.enums;
+
+public enum Status {
+	PENDENTE,
+	EM_ANDAMENTO,
+	CONCLUIDO;
+
+}
