bd4c79a488ac82802837c0fd35ad4933d1ea55c2
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-12 23:47:49 -0300

    tarefa findById

--- src/main/java/com/evtechsolution/gerenciador_tarefas/resources/TarefaResource.java
+++ src/main/java/com/evtechsolution/gerenciador_tarefas/resources/TarefaResource.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.evtechsolution.gerenciador_tarefas.entities.Tarefa;
 import com.evtechsolution.gerenciador_tarefas.services.TarefaService;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping(value = "/tarefas")
@@ -22,6 +28,13 @@ public class TarefaResource {
 	public ResponseEntity<List<Tarefa>> findAll(){
 		List<Tarefa> list = tarefaService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tarefa> findById(@PathVariable Long id){
+		Tarefa obj = tarefaService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 
