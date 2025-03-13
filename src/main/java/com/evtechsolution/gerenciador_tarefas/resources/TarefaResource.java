9223b4a57fefb20aa5997010f8f53f8160a310da
Evandro Sacramento II <evandro-dev@outlook.com>
2025-03-13 00:02:22 -0300

    update

--- src/main/java/com/evtechsolution/gerenciador_tarefas/resources/TarefaResource.java
+++ src/main/java/com/evtechsolution/gerenciador_tarefas/resources/TarefaResource.java
@@ -1,17 +1,22 @@
 package com.evtechsolution.gerenciador_tarefas.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.evtechsolution.gerenciador_tarefas.entities.Tarefa;
 import com.evtechsolution.gerenciador_tarefas.services.TarefaService;
 
+import jakarta.servlet.Servlet;
 import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,5 +42,11 @@ public class TarefaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Tarefa> insert(@RequestBody Tarefa obj){
+		obj = tarefaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(null).buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 
 }
